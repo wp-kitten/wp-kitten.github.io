3791f2e37a3f9a10e31801ff6633a83fa5d7a6da
wp-kitten <wp.kytten@gmail.com>
2014-12-15 12:59:17 +0200

    Changing layout Adding projects

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+var WpProjects = [
+    {
+        'title': 'STW Demo Plugin',
+        'description' : 'Provides the basic template for a WordPress plugin',
+        'url' : 'https://github.com/wp-kitten/stw-demo-plugin'
+    }
+    ,{
+        'title': 'Wp Admin Submenu Page Bubble',
+        'description' : 'Custom function to use when creating submenu pages where you need to display a count bubble',
+        'url' : 'https://github.com/wp-kitten/wp-admin-submenu-page-bubble'
+    }
+    ,{
+        'title': 'WPK Notifications',
+        'description' : 'Provides an easy way for WordPress plugin developers to display admin notifications',
+        'url' : 'https://github.com/wp-kitten/WPK-Notifications'
+    }
+];
