3bef52afd380ce9010de847aa1a55da5157a1d5e
Costin <wp.kytten@gmail.com>
2015-02-15 04:38:25 +0200

    Added the Relocate Publish Button plugin

--- javascripts/main.js
+++ javascripts/main.js
@@ -24,4 +24,9 @@ var WpProjects = [
         'description' : 'This is a fork of PHP Hooks Class (https://github.com/bainternet/PHP-Hooks/) which extracted the hook functionality from WordPress. This version, unlike the original, is providing the action and filter functions globally, just like WordPress does.',
         'url' : 'https://github.com/wp-kitten/PHP-Hooks'
     }
+    ,{
+        'title': 'Relocate Publish Button',
+        'description' : "Relocate the [post's, page's] Publish/update button to the admin bar. Useful when the page contains many elements and requires a lot of scrolling in order to publish/update the post/page.",
+        'url' : 'https://github.com/wp-kitten/relocate-publish-update-button'
+    }
 ];
